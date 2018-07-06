e5a4f0a5b743c337867c6ac1eca1184dca10fc23
Kami Makoś <makos.kamil@o2.pl>
2018-07-06 08:41:03 +0200

    add_files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
 
-app.use(function(req, res, next){
-    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
 });
 
@@ -15,9 +17,11 @@ app.get('/store', function (req, res) {
     res.send('To jest sklep');
 });
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
+app.get('/first-template, function(req, res){
+    res.render('first-template');
+});
 
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
